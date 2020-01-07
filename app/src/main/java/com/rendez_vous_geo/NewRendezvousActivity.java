a801d4ae7a5768278ed33064aba7c45c1f5ce5dd
Adrien Chinour <adrien.chinour@protonmail.com>
2020-01-07 22:47:35 +0100

    Ajout du datepicker et timepicker + amélioration layout

--- app/src/main/java/com/rendez_vous_geo/NewRendezvousActivity.java
+++ app/src/main/java/com/rendez_vous_geo/NewRendezvousActivity.java
@@ -1,5 +1,8 @@
 package com.rendez_vous_geo;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.location.Address;
@@ -7,18 +10,26 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.text.Editable;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.schibstedspain.leku.LocationPickerActivity;
 
-import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Calendar;
 import java.util.Locale;
+import java.util.Objects;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 /**
  * Activité de création d'un rdv géolocalisé
@@ -31,6 +42,9 @@ public class NewRendezvousActivity extends AppCompatActivity {
     static final int PICK_LOCATION_REQUEST = 2; // Code requete pour l'intent de choix lieu
 
     private LatLng latLng;
+
+    private Date date;
+    private Time time;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -112,7 +126,7 @@ public class NewRendezvousActivity extends AppCompatActivity {
 
                 // récupération de l'adresse complète pour l'affichage
                 Address address = data.getParcelableExtra("address");
-                TextView addressOutput = findViewById(R.id.location_info);
+                TextView addressOutput = findViewById(R.id.location);
                 addressOutput.setText(address.getAddressLine(0));
             }
         }
@@ -156,4 +170,61 @@ public class NewRendezvousActivity extends AppCompatActivity {
         return intent;
     }
 
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            NewRendezvousActivity activity = (NewRendezvousActivity) getActivity();
+            Objects.requireNonNull(activity).time = new Time(hourOfDay, minute, 0);
+            Button time = activity.findViewById(R.id.time_picker);
+
+            String formattedTime = String.format(Locale.FRANCE, "%d:%d", hourOfDay, minute);
+            time.setText(formattedTime);
+        }
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            NewRendezvousActivity activity = (NewRendezvousActivity) getActivity();
+            Objects.requireNonNull(activity).date = new Date(year, month, day);
+            Button date = activity.findViewById(R.id.date_picker);
+
+            String formattedDate = String.format(Locale.FRANCE, "%d/%d/%d", day, month + 1, year);
+            date.setText(formattedDate);
+        }
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
 }
