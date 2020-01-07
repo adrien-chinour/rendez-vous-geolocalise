094587469b88b7aefec340f06b95bd873f718e40
Adrien Chinour <adrien.chinour@protonmail.com>
2020-01-07 21:12:36 +0100

    Ajout de la sélection d'une localisation

--- app/src/main/java/com/rendez_vous_geo/NewRendezvousActivity.java
+++ app/src/main/java/com/rendez_vous_geo/NewRendezvousActivity.java
@@ -2,16 +2,20 @@ package com.rendez_vous_geo;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Address;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.schibstedspain.leku.LocationPickerActivity;
-import com.schibstedspain.leku.locale.SearchZoneRect;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,10 +30,11 @@ public class NewRendezvousActivity extends AppCompatActivity {
     static final int PICK_CONTACT_REQUEST = 1; // Code requete pour l'intent de choix contact
     static final int PICK_LOCATION_REQUEST = 2; // Code requete pour l'intent de choix lieu
 
+    private LatLng latLng;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO créer le layout selon la maquette de NewRendezvousActivity
         setContentView(R.layout.activity_new_rendezvous);
     }
 
@@ -44,7 +49,19 @@ public class NewRendezvousActivity extends AppCompatActivity {
         startActivityForResult(intent, PICK_CONTACT_REQUEST);
     }
 
+    public void addLocationAction(View view) {
+        Intent locationPickerIntent = new LocationPickerActivity.Builder()
+                .withDefaultLocaleSearchZone()
+                .shouldReturnOkOnBackPressed()
+                .build(getApplicationContext());
 
+        startActivityForResult(locationPickerIntent, PICK_LOCATION_REQUEST);
+    }
+
+    public void previewAction(View view) {
+        TextView preview = findViewById(R.id.msg_preview);
+        preview.setText(getMessage());
+    }
 
     /**
      * Action du bouton de l'envoi du rdv
@@ -87,17 +104,56 @@ public class NewRendezvousActivity extends AppCompatActivity {
 
         if (requestCode == PICK_LOCATION_REQUEST) {
             if (resultCode == RESULT_OK) {
-                System.out.println("result");
+
+                this.latLng = new LatLng(
+                        data.getDoubleExtra("latitude", 0.0),
+                        data.getDoubleExtra("longitude", 0.0)
+                );
+
+                // récupération de l'adresse complète pour l'affichage
+                Address address = data.getParcelableExtra("address");
+                TextView addressOutput = findViewById(R.id.location_info);
+                addressOutput.setText(address.getAddressLine(0));
             }
         }
     }
 
-    public void addLocationAction(View view) {
-        Intent locationPickerIntent = new LocationPickerActivity.Builder()
-                .withDefaultLocaleSearchZone()
-                .shouldReturnOkOnBackPressed()
-                .build(getApplicationContext());
+    /**
+     * Écriture du SMS à envoyer au(x) récepteur(s)
+     *
+     * @return String message
+     */
+    private String getMessage() {
+        TextView message = findViewById(R.id.message);
+        TextView address = findViewById(R.id.location_info);
 
-        startActivityForResult(locationPickerIntent, PICK_LOCATION_REQUEST);
+        // Ajout d'un message personnalisé avant le contenu du SMS si l'utilisateur en a saisi un
+        String content = "";
+        if (message.getText().length() != 0) {
+            content = message.getText() + System.lineSeparator();
+        }
+
+        // On retourne le message à envoyer au(x) récepteur(s)
+        return content +
+                "Lieu du rendez-vous : " + address.getText() +
+                System.lineSeparator() +
+                "Pour valider le rendez-vous et avoir plus d'informations : ";
     }
+
+    /**
+     * Construction de l'intent pour le(s) destinataire(s) du rdv
+     *
+     * @return Intent intent à envoyer au(x) destinataire(s)
+     */
+    private Intent getValidationIntent() {
+
+        Intent intent = new Intent();
+        intent.setAction("com.rendez_vous_geo.VALIDATION");
+        intent.putExtra("latitude", latLng.latitude);
+        intent.putExtra("longitude", latLng.longitude);
+        // TODO récupérer numéro + date
+
+        return intent;
+    }
+
 }
