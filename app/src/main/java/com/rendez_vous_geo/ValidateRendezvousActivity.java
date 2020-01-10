451e84e0f073ec0e99c56c140bf4d017fa31cf91
Adrien Chinour <adrien.chinour@protonmail.com>
2020-01-10 01:24:08 +0100

    Amélioration UI + UX pour la page de validation de rdv

--- app/src/main/java/com/rendez_vous_geo/ValidateRendezvousActivity.java
+++ app/src/main/java/com/rendez_vous_geo/ValidateRendezvousActivity.java
@@ -1,39 +1,56 @@
 package com.rendez_vous_geo;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import com.google.android.gms.maps.model.LatLng;
-
 
 import static android.Manifest.permission.SEND_SMS;
 
 /**
  * Activité permettant de d'accepter ou non un rdv
  */
-public class ValidateRendezvousActivity extends AppCompatActivity {
+public class ValidateRendezvousActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
-    private static final String ACCEPT = "Accepter";
+
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundle";
+
+    private MapView mapView;
+
+    private boolean accepted;
 
     // Attributs récupérés du SMS
     private LatLng latLng;
     private String phoneNumber = null;
-    private String adress = null;
+    private String address = null;
     private String date = null;
     private String time = null;
 
@@ -42,83 +59,177 @@ public class ValidateRendezvousActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate_rendezvous);
 
-        // récupération des queryParams du lien cliqué
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        addViewData();
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+    }
+
+    private void addViewData() {
         Uri dataFromLink = getIntent().getData();
         this.latLng = new LatLng(
                 Double.parseDouble(dataFromLink.getQueryParameter("latitude")),
                 Double.parseDouble(dataFromLink.getQueryParameter("longitude")));
         this.phoneNumber = dataFromLink.getQueryParameter("phone");
-        this.adress = dataFromLink.getQueryParameter("adress");
+        this.address = dataFromLink.getQueryParameter("adress");
         this.date = dataFromLink.getQueryParameter("date");
         this.time = dataFromLink.getQueryParameter("time");
 
-        // Affichage des attributs dans l'activité
-        TextView tvPhoneNumber = findViewById(R.id.tvPhoneNumber);
-        tvPhoneNumber.setText(this.phoneNumber);
-        TextView tvAdress = findViewById(R.id.tvAdress);
-        tvAdress.setText(this.adress);
-        TextView tvDate = findViewById(R.id.tvDate);
-        tvDate.setText(this.date);
-        TextView tvTime = findViewById(R.id.tvTime);
-        tvTime.setText(this.time);
+        TextView title = findViewById(R.id.display_title);
+        title.setText("Nouveau rendez-vous reçu du " + this.phoneNumber);
+
+        TextView date = findViewById(R.id.display_date);
+        date.setText("Rendez-vous le " + this.date + " à " + this.time);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    public void createEventAction(View view) {
+        DateTime dateTime;
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
+            dateTime = formatter.parseDateTime(date);
+        } catch (Exception e) {
+            Toast.makeText(this, "Impossible de créer un évenement.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, dateTime.millisOfSecond())
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, address);
+
+        startActivity(intent);
     }
 
     /**
-     * Fonction permettant d'envoyer le SMS de réponse selon le bouton cliqué
-     * @param view
+     * Méthode de validation du rdv
+     *
+     * @param view : bouton accepter
      */
-    public void responseAction(View view) {
-        Button clickedButton = findViewById(view.getId());
-        String choix;
-
-        // Construction du message
-        if (clickedButton.getText().equals(ACCEPT)) {
-            choix = "accepté";
-        }
-        else {
-            choix = "refusé";
-        }
-
-        String message = "Le rendez-vous au " + this.adress +
-                " le " + this.date +
-                " à " + this.time + " a été " + choix + ".";
-        // Envoi du message
-        sendSMS(message);
-        Toast.makeText(getApplicationContext(), "Votre message a bien été envoyé.", Toast.LENGTH_LONG).show();
+    public void validateAction(View view) {
+        accepted = true;
+        sendSMS();
     }
 
     /**
-     * Fonction appelée au clic sur le bouton "Ouvrir dans Google Maps" permettant d'ouvrir
-     * l'application "Google Maps" avec la latitude et la longitude définies dans le rdv
-     * @param v
+     * Méthode de refus du rdv
+     *
+     * @param view : bouton refuser
      */
-    public void openMaps(View v) {
-        // Uri permettant de renseigner la latitude et la longitude
-        Uri uriLatLong = Uri.parse("geo:0,0?q=" + latLng.latitude + "," + latLng.longitude);
-        // Définition de l'intent
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriLatLong);
-        // On ouvre l'intent avec l'application Google Maps
-        mapIntent.setPackage("com.google.android.apps.maps");
-        // Démarrage de l'activité
-        startActivity(mapIntent);
+    public void refuseAction(View view) {
+        accepted = false;
+        sendSMS();
     }
 
     /**
      * Fonction permettant d'envoyer un SMS
-     * @param message
      */
-    public void sendSMS(String message) {
-        checkSMSPermission();
-        SmsManager sm = SmsManager.getDefault();
-        sm.sendTextMessage(this.phoneNumber, null, message, null, null);
-    }
+    @SuppressLint("StringFormatMatches")
+    public void sendSMS() {
 
-    /**
-     * Fonction permettant de vérifier si la permission d'envoi des SMS a été donnée
-     */
-    public void checkSMSPermission() {
+        // vérification des permissions pour l'envoi du sms
         if (ContextCompat.checkSelfPermission(this, SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+        } else {
+            SmsManager sm = SmsManager.getDefault();
+            ArrayList<String> parts = sm.divideMessage(getMessage());
+            sm.sendMultipartTextMessage(this.phoneNumber, null, parts, null, null);
+        }
+
+        // retour a l'activité main
+        Toast.makeText(getApplicationContext(), "Votre message a bien été envoyé.", Toast.LENGTH_LONG).show();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private String getMessage() {
+        String message = "Le rendez-vous au " + address + " le " + date + " à " + time;
+        if (accepted) {
+            return message + " a été accepté.";
+        }
+
+        return message + " a été refusé.";
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_SEND_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSMS();
+            } else {
+                Toast.makeText(this, getString(R.string.error_permission_sms), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMinZoomPreference(15);
+        if (googleMap != null) {
+            googleMap.addMarker(new MarkerOptions()
+                    .position(latLng).title(address)
+                    .icon(BitmapDescriptorFactory.defaultMarker())
+                    .draggable(false).visible(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         }
     }
 }
