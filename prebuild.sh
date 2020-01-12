b2d7a2a34d3020b1a764b8e2ec3578dd44f343fb
Adrien Chinour <adrien.chinour@protonmail.com>
2020-01-12 15:55:04 +0100

    Fix du workflow android.yml

--- prebuild.sh
+++ prebuild.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Permet de générer le fichier secrets.xml pour la CI
-envsubst < app/src/main/res/values/secrets.xml.exemple > app/src/main/res/values/secrets.xml
+envsubst < ./.ci/secrets.xml.exemple > app/src/main/res/values/secrets.xml
